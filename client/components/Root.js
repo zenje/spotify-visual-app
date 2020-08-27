ea0136ef2aae7d4ef56033d20aa7544e4e98200c
Jessica Zeng <jessicaczeng@gmail.com>
2020-08-27 10:12:25 +0000

    use react-helmet to load head, remove font import from global style

--- client/components/Root.js
+++ client/components/Root.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Cookies from 'js-cookie';
 import { ThemeProvider } from 'styled-components';
 import { StylesProvider } from '@material-ui/core/styles';
+import { Helmet } from 'react-helmet';
 
 import theme from '../styles/theme';
 import GlobalStyle from '../styles/global';
@@ -16,27 +17,38 @@ const refreshToken = Cookies.get('spotifyRefreshToken');
 const setCookies = false;
 
 const Root = () => (
-  <StylesProvider injectFirst>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            {accessToken ? (
-              <Redirect
-                to={`/user/${accessToken}/${refreshToken}/${setCookies}`}
-              />
-            ) : (
-              <Login />
-            )}
-          </Route>
-          <Route path="/user/:accessToken/:refreshToken/:setCookies">
-            <User />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </ThemeProvider>
-  </StylesProvider>
+  <>
+    <Helmet>
+      <title>fun with spotify</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <StylesProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              {accessToken ? (
+                <Redirect
+                  to={`/user/${accessToken}/${refreshToken}/${setCookies}`}
+                />
+              ) : (
+                <Login />
+              )}
+            </Route>
+            <Route path="/user/:accessToken/:refreshToken/:setCookies">
+              <User />
+            </Route>
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </StylesProvider>
+  </>
 );
 
 export default Root;
--- client/styles/global.js
+++ client/styles/global.js
@@ -1,10 +1,9 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Work+Sans:ital,wght@0,400;0,700;1,400&display=swap');
   body {
     background-color: ${(props) => props.theme.colors.background};
-    font-family: 'Space Mono', monospace;
+    font-family: 'Courier Prime', monospace;
     font-size: 16px;
     color: ${(props) => props.theme.colors.text};
   }
