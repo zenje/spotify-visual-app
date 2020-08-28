0dfa21f16e8e2378a3e1f678548a6b83b07f9672
Jessica Zeng <jessicaczeng@gmail.com>
2020-08-28 17:42:02 +0000

    update Login component, change styling

---
+++ client/components/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import loginSVG from '../../log_in.svg';
+
+import {SpotifySVG, SpotifyWrapper, StyledContainer as Container, Wrapper} from './style';
+
+ export default function Login() {
+    return (
+      <Container><Wrapper>
+        <h2>hi, friend! please login to begin.</h2>
+        <SpotifyWrapper><SpotifySVG><a href="/login" dangerouslySetInnerHTML={{ __html: loginSVG }}></a></SpotifySVG></SpotifyWrapper>
+      </Wrapper>
+      </Container>
+    );
+}
