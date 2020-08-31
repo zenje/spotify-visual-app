482dfdffb238fa81f94882894289b1a726f06706
Jessica Zeng <jessicaczeng@gmail.com>
2020-08-31 09:46:34 +0000

    Updates

--- client/components/Main/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import Container from '@material-ui/core/Container';
-
-export const StyledContainer = styled(Container)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 90vh;
-`;
