53fddfc716e28d04491ffc7d8850bd3a8aa55d98
Jessica Zeng <jessicaczeng@gmail.com>
2020-11-23 05:50:19 +0000

    strip track title before fetching track lyrics

--- server/api/geniusLyrics.js
+++ server/api/geniusLyrics.js
@@ -8,7 +8,7 @@ router.get('/genius/:track/:artist', async (req, res) => {
   const { artist, track } = req.params;
   const client = new GeniusFetcher.Client(ACCESS_TOKEN);
   client
-    .fetch(track, artist)
+    .fetch(stripTrack(track), artist)
     .then((result) => res.status(200).json(result))
     .catch((error) => res.status(404).send({ error: error.toString() }));
 });
@@ -21,5 +21,21 @@ router.get('/genius/:artist', async (req, res) => {
     .then((result) => res.status(200).json(result))
     .catch((error) => res.status(404).send({ error: error.toString() }));
 });
+
+// get base track name, removing any classifiers from Spotify track title
+const stripTrack = (trackTitle) => {
+  // truncate tracks featuring another artist
+  const featIndex = trackTitle.indexOf(' (feat.');
+  if (featIndex > -1) {
+    trackTitle = trackTitle.slice(0, featIndex);
+  }
+
+  // truncate any additional classifiers after hyphen (e.g., '...Remix', '...Version')
+  const hyphenIndex = trackTitle.indexOf(' - ');
+  if (hyphenIndex > -1) {
+    trackTitle = trackTitle.slice(0, hyphenIndex);
+  }
+  return trackTitle;
+};
 
 module.exports = router;
